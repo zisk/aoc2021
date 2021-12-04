153e9aa24934af077c2bdd9e420a031ec2c6a3fe
Ryan Zisk <ryan@ryanzisk.com>
2021-12-04 10:12:50 -0500

    Day 3 Part 2

--- day3/day3.go
+++ day3/day3.go
@@ -8,22 +8,25 @@ import (
 	"github.com/zisk/aoc2021/util"
 )
 
-func main() {
-	input, _ := util.InputToTxt()
+func BitPopularity(input []string) map[int][]int {
+	bitMap := make(map[int][]int)
 
-	bitCount := make(map[int][]int)
-
-	// populate the lists
 	for i := 0; i < len(input[0]); i++ {
-		bitCount[i] = []int{0, 0}
+		bitMap[i] = []int{0, 0}
 	}
 
 	for _, line := range input {
 		for i, bit := range strings.Split(line, "") {
 			valueToInt, _ := strconv.Atoi(bit)
-			bitCount[i][valueToInt]++
+			bitMap[i][valueToInt]++
 		}
 	}
+	return bitMap
+}
+func main() {
+	input, _ := util.InputToTxt()
+
+	bitCount := BitPopularity(input)
 
 	var gamma string
 	var epsilon string
@@ -41,8 +44,65 @@ func main() {
 	gammaInt, _ := strconv.ParseInt(gamma, 2, 64)
 	epsilonInt, _ := strconv.ParseInt(epsilon, 2, 64)
 
-	fmt.Println(bitCount)
 	fmt.Printf("\nGamma: %s\nEpsilon: %s\n\n", gamma, epsilon)
 	fmt.Printf("Gamma: %d\nEpsilon: %d\n\n", gammaInt, epsilonInt)
-	fmt.Printf("Power Consumption: %d\n", gammaInt*epsilonInt)
+	fmt.Printf("Power Consumption: %d\n\n", gammaInt*epsilonInt)
+
+	oxResult := make([]string, len(input))
+	_ = copy(oxResult, input)
+
+	for i := 0; i < len(input[0]); i++ {
+		oxPop := BitPopularity(oxResult)
+		b := 0
+		if oxPop[i][0] == oxPop[i][1] {
+			b = 1
+		} else if oxPop[i][0] < oxPop[i][1] {
+			b = 1
+		}
+
+		var oxHold []string
+		for _, n := range oxResult {
+			if strings.Split(n, "")[i] == strconv.Itoa(b) {
+				oxHold = append(oxHold, n)
+			}
+		}
+
+		oxResult = oxHold
+
+		if len(oxResult) == 1 {
+			break
+		}
+	}
+
+	coResult := make([]string, len(input))
+	_ = copy(coResult, input)
+
+	for i := 0; i < len(input[0]); i++ {
+		coPop := BitPopularity(coResult)
+		b := 1
+		if coPop[i][0] == coPop[i][1] {
+			b = 0
+		} else if coPop[i][0] < coPop[i][1] {
+			b = 0
+		}
+
+		var coHold []string
+		for _, n := range coResult {
+			if strings.Split(n, "")[i] == strconv.Itoa(b) {
+				coHold = append(coHold, n)
+			}
+		}
+
+		coResult = coHold
+
+		if len(coResult) == 1 {
+			break
+		}
+	}
+
+	oxInt, _ := strconv.ParseInt(oxResult[0], 2, 64)
+	coInt, _ := strconv.ParseInt(coResult[0], 2, 64)
+	fmt.Printf("Oxygen Rate: %d\n", oxInt)
+	fmt.Printf("C02 Rate: %d\n", coInt)
+	fmt.Printf("Life support rating: %d\n", oxInt*coInt)
 }
