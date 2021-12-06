a6934b76ee8ca820efe8d14232bc22ce89a45509
Ryan Zisk <ryan@ryanzisk.com>
2021-12-06 08:24:43 -0500

    Day 5 Part 2

--- day5/day5.go
+++ day5/day5.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 
@@ -22,6 +23,13 @@ func min(a int, b int) int {
 	return b
 }
 
+func neg(a int) int {
+	if a > 0 {
+		return 1
+	}
+	return -1
+}
+
 type point struct {
 	x, y int
 }
@@ -31,7 +39,7 @@ type line struct {
 	end   point
 }
 
-func (l *line) trace() []point {
+func (l *line) trace(diag bool) []point {
 	var lineTrace []point
 	if l.start.x != l.end.x && l.start.y == l.end.y {
 		highPoint := max(l.start.x, l.end.x)
@@ -44,6 +52,16 @@ func (l *line) trace() []point {
 		lowPoint := min(l.start.y, l.end.y)
 		for p := lowPoint; p <= highPoint; p++ {
 			lineTrace = append(lineTrace, point{x: l.start.x, y: p})
+		}
+	} else if diag {
+		slopeX := neg(l.end.x - l.start.x)
+		slopeY := neg(l.end.y - l.start.y)
+		diffX := int(math.Abs(float64(l.start.x) - float64(l.end.x)))
+
+		for p := 0; p <= diffX; p++ {
+			distX := l.start.x + (p * slopeX)
+			distY := l.start.y + (p * slopeY)
+			lineTrace = append(lineTrace, point{x: distX, y: distY})
 		}
 	}
 	return lineTrace
@@ -67,28 +85,47 @@ func main() {
 		lines = append(lines, newLine(line))
 	}
 
-	// fmt.Println(lines)
+	// Part 1
 	var points []point
 	for _, ls := range lines {
-		newPoints := ls.trace()
+		newPoints := ls.trace(false)
 		if len(newPoints) != 0 {
 			points = append(points, newPoints...)
 		}
 	}
 
-	dupeMap := make(map[string]int)
+	dupeMap1 := make(map[string]int)
 
 	for _, ps := range points {
 		k := fmt.Sprintf("%03d:%03d", ps.x, ps.y)
-		dupeMap[k]++
+		dupeMap1[k]++
 	}
 
-	overCount := 0
-	for _, m := range dupeMap {
+	overCount1 := 0
+	for _, m := range dupeMap1 {
 		if m > 1 {
-			overCount++
+			overCount1++
 		}
 	}
 
-	fmt.Printf("Overlapping Points: %d\n", overCount)
+	// Part 2
+	var diagPoints []point
+	diagMap := make(map[string]int)
+	overCount2 := 0
+	for _, ls := range lines {
+		ps := ls.trace(true)
+		diagPoints = append(diagPoints, ps...)
+	}
+	for _, ps := range diagPoints {
+		k := fmt.Sprintf("%03d:%03d", ps.x, ps.y)
+		diagMap[k]++
+	}
+	for _, m := range diagMap {
+		if m > 1 {
+			overCount2++
+		}
+	}
+
+	fmt.Printf("Part 1: %d\n", overCount1)
+	fmt.Printf("Part 2: %d\n", overCount2)
 }
