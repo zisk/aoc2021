6285c25f058fc1309e1f3362965fe2f09a94fdef
Ryan Zisk <ryan@ryanzisk.com>
2021-12-14 17:38:52 -0500

    Day 9 Part 2

--- day9/day9.go
+++ day9/day9.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/zisk/aoc2021/util"
 )
 
-func getAdjacent(m map[int][]int, x int, y int) []int {
+type point struct {
+	x int
+	y int
+}
+
+func getAdjacentValues(m map[int][]int, x int, y int) []int {
 	var r []int
 	// up
 	if x > 0 {
@@ -29,6 +35,75 @@ func getAdjacent(m map[int][]int, x int, y int) []int {
 	return r
 }
 
+func getAdjacentNodes(m map[int][]int, p point) []point {
+	var r []point
+	// up
+	if p.x > 0 {
+		r = append(r, point{x: p.x - 1, y: p.y})
+	}
+	// right
+	if (p.y + 1) < len(m[p.x]) {
+		r = append(r, point{x: p.x, y: p.y + 1})
+	}
+	// left
+	if p.y > 0 {
+		r = append(r, point{x: p.x, y: p.y - 1})
+	}
+	// down
+	if (p.x + 1) < len(m) {
+		r = append(r, point{x: p.x + 1, y: p.y})
+	}
+	return r
+}
+
+func popNode(s []point) ([]point, point) {
+	l := len(s)
+	n := s[l-1]
+	if l == 1 {
+		return make([]point, 0), n
+	}
+	return s[:len(s)-1], n
+}
+
+func checkVisited(v []point, n point) bool {
+	for _, p := range v {
+		if p.x == n.x && p.y == n.y {
+			return true
+		}
+	}
+	return false
+}
+
+func mapBasin(m map[int][]int, start point) []point {
+	var next point
+	stack := make([]point, 1)
+	stack[0] = start
+	visited := make([]point, 0)
+	for len(stack) > 0 {
+		stack, next = popNode(stack)
+		newNodes := getAdjacentNodes(m, next)
+		for _, node := range newNodes {
+			nodeVal := m[node.x][node.y]
+			if !(checkVisited(visited, node)) && nodeVal != 9 {
+				stack = append(stack, node)
+			}
+		}
+		if !(checkVisited(visited, next)) {
+			visited = append(visited, next)
+		}
+	}
+	return visited
+}
+
+func getTopBasins(m map[int][]int, basins [][]point) []int {
+	var h []int
+	for _, p := range basins {
+		h = append(h, len(p))
+	}
+	sort.Ints(h)
+	return h[len(h)-3:]
+}
+
 func isLowPoint(v int, adj []int) bool {
 	for _, i := range adj {
 		if v >= i {
@@ -46,14 +121,26 @@ func main() {
 	}
 
 	risks := 0
+	lowPoints := make([]point, 0)
 	for row := 0; row < len(heightMap); row++ {
 		for col := range heightMap[row] {
-			point := heightMap[row][col]
-			adjNums := getAdjacent(heightMap, row, col)
-			if isLowPoint(point, adjNums) {
-				risks += point + 1
+			p := heightMap[row][col]
+			adjNums := getAdjacentValues(heightMap, row, col)
+			if isLowPoint(p, adjNums) {
+				risks += p + 1
+				lowPoints = append(lowPoints, point{x: row, y: col})
 			}
 		}
 	}
 	fmt.Printf("Part 1: %d\n", risks)
+
+	var basins [][]point
+	for _, lowPoint := range lowPoints {
+		b := mapBasin(heightMap, lowPoint)
+		basins = append(basins, b)
+	}
+	topThree := getTopBasins(heightMap, basins)
+	basinTotal := topThree[0] * topThree[1] * topThree[2]
+	fmt.Printf("Top Three Basins: %v\n", topThree)
+	fmt.Printf("Part 2: %d\n", basinTotal)
 }
